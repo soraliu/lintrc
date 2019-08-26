87179885f9a22a5d9b40e3468c0810c033c17207
Sora Liu <sora.liu@binance.com>
2019-08-26 11:12:43 +0800

    feat(eslint): init eslint config, add scripts

---
+++ eslint-example.js
@@ -0,0 +1,16 @@
+for (let i = 0; i < 10; i++) {
+  let p = new Promise((res, rej) => {
+    console.log(rej);
+
+    /**
+     * array
+     */
+    let arr = [
+      1,
+      2,
+    ];
+  });
+
+  console.log(p);
+}
+
