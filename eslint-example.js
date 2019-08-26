5dca9d1c6d0492937aec38514543e1d8774fcf1b
Sora Liu <sora.liu@binance.com>
2019-08-26 11:40:58 +0800

    feat(eslint): add some rules

--- eslint-example.js
+++ eslint-example.js
@@ -1,6 +1,20 @@
 for (let i = 0; i < 10; i++) {
   let p = new Promise((res, rej) => {
     console.log(rej);
+    rej('1');
+
+    if (i) {
+      console.log(true);
+    }
+
+    switch (i) {
+      case 1: {
+        console.log(1);
+      }
+    }
+
+    let fn = (b, a) => b + a;
+    console.log(fn);
 
     /**
      * array
@@ -12,5 +26,8 @@ for (let i = 0; i < 10; i++) {
   });
 
   console.log(p);
+
+  // space
+  console.log(1 + 2);
 }
 
